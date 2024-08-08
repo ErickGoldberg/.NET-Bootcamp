c52a828e859036917d80de6b2b59a14e16dbd7b2
Erick Goldberg <erick_goldberg@hotmail.com>
2024-08-07 22:52:15 -0300

    Abstracting cellphone challenge

---
+++ AbstractingCellphone/AbstractingCellphone/Program.cs
@@ -0,0 +1,21 @@
+﻿using AbstractingCellphone.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "111111111111111", memoria: 64);
+        Iphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 12", imei: "222222222222222", memoria: 128);
+
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+    }
+}
